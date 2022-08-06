d19f451ad2d14c7e46079f099ff6117fc20d8738
darrida <darrida.py@gmail.com>
2022-08-06 10:07:06 -0500

    update

--- create_block.py
+++ create_block.py
@@ -3,20 +3,28 @@ from smb_block import SMB
 
 
 # smb = RemoteFileSystem(
-#     basepath='smb://SHARE/path_dir/path_dir/',
+#     basepath='smb://prefect_storage/test/',
 #     settings={
-#         'host': 'server.com',
-#         'username': '<USERNAME>',
-#         'password': '<PASSWORD>',
+#         'host': 'pi4-main',
+#         'username': 'pi',
+#         'password': 'Qf6DJBecymsQKa4xwFmviqYHQfewxz',
 #     }
 # )
-# smb.save("temp", overwrite=True)
+# smb.save("test-smb", overwrite=True)
 
 
 smb = SMB(
-    share_path='/SHARE/path_dir/path_dir/',
-    host='server.com',
-    smb_access_username='<USERNAME>',
-    smb_access_password='<PASSWORD>',
+    share_path='/share/', #'/SHARE/path_dir/path_dir/',
+    host='127.0.0.1', #'server.com',
+    port=444,
+    smb_access_username='web',
+    smb_access_password='new',
 )
 smb.save("test-smb", overwrite=True)
+
+
+# docker run -it -v/var/www:/share/web -p 444:445 -e sharename=web -e password=1234578 ahmetozer/samba
+# useradd
+# web
+# new
+# reload-samba
