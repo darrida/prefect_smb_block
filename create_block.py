b3a682771aba0824df538ceacb037939102a776d
darrida <darrida.py@gmail.com>
2022-08-07 11:07:47 -0500

    finally figured out that smb needs to be setup on Windows, not linux

--- create_block.py
+++ create_block.py
@@ -1,24 +1,26 @@
 from prefect.filesystems import RemoteFileSystem
 from smb_block import SMB
 
+# have to comment out "overwrite=True" on line 298
+# poetry run prefect deployment build test_flow.py:test_smb -sb remote-file-system/test-smb --name smb-test
+smb = RemoteFileSystem(
+    basepath='smb://benha/smb_test/',
+    settings={
+        'host': 'benha',
+        'username': 'web',
+        'password': '<password>',
+    }
+)
+smb.save("test-smb", overwrite=True)
 
-# smb = RemoteFileSystem(
-#     basepath='smb://prefect_storage/test/',
-#     settings={
-#         'host': 'pi4-main',
-#         'username': 'pi',
-#         'password': 'Qf6DJBecymsQKa4xwFmviqYHQfewxz',
-#     }
-# )
-# smb.save("test-smb", overwrite=True)
-
-
+# had to made adjustment to filesystems.py (adjusted file in this repo) to get this to work
+# poetry run prefect deployment build test_flow.py:test_smb -sb smb/test-smb --name smb-test
 smb = SMB(
-    share_path='/share/', #'/SHARE/path_dir/path_dir/',
-    host='127.0.0.1', #'server.com',
-    port=444,
+    share_path='/smb_test/', #'/SHARE/path_dir/path_dir/',
+    host='benha', #'server.com',
+    port=445,
     smb_access_username='web',
-    smb_access_password='new',
+    smb_access_password='<password>',
 )
 smb.save("test-smb", overwrite=True)
 
